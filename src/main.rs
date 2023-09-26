78ee163d7540d0a74b9b241948d7270c6448c928
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-26 17:14:38 +0300

    🎉 feat(main.rs): Implement DNA Animation with Ctrl-C Quit Functionality 📝 Summary: Added DNA animation functionality to the application. Users can now enjoy a dynamic DNA sequence animation that can be interrupted by pressing Ctrl-C. 💡 Details: - Created a `Dna` struct that implements the `Animation` trait to handle DNA sequence animation. - Implemented logic for continuous animation until interrupted by the user. - Integrated a Ctrl-C handler to gracefully exit the animation when Ctrl-C is pressed. - Utilized random nucleotide pairing for the DNA sequence animation. - Enhanced code readability and maintainability. ➕ Dependencies: - Added the `ctrlc` and `rand` dependencies to handle Ctrl-C interrupts and random nucleotide selection, respectively. 🔍 Context: - The application now provides an engaging DNA sequence animation that simulates the pairing of nucleotides. Users can observe the animation until they choose to exit by pressing Ctrl-C. 🎉 New Feature: - The newly added feature allows users to experience an animated representation of a DNA sequence. It incorporates random nucleotide pairing and graceful termination via Ctrl-C. 📚 Challenges: - N/A ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,97 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+trait Animation {
+    fn animate(&self, running: &AtomicBool);
+}
+
+struct Dna {
+    pause: Duration,
+    rows: [&'static str; 18],
+}
+
+impl Animation for Dna {
+    fn animate(&self, running: &AtomicBool) {
+        let mut row_index = 0;
+        let rows_len = self.rows.len();
+        while running.load(Ordering::SeqCst) {
+            row_index = (row_index + 1) % rows_len;
+
+            let row = if row_index == 0 || row_index == 9 {
+                self.rows[row_index].to_owned()
+            } else {
+                let n = match rand::random::<u8>() % 4 {
+                    0 => ('A', 'T'),
+                    1 => ('T', 'A'),
+                    2 => ('C', 'G'),
+                    3 => ('G', 'C'),
+                    _ => unreachable!(),
+                };
+                self.rows[row_index]
+                    .replace("{}", &n.0.to_string())
+                    .replace("{}", &n.1.to_string())
+            };
+            println!("{}", row);
+            thread::sleep(self.pause);
+        }
+    }
+}
+
+struct App {
+    animation: Box<dyn Animation>,
+}
+
+impl App {
+    fn new(animation: Box<dyn Animation>) -> Self {
+        Self { animation }
+    }
+
+    fn run(&self, running: Arc<AtomicBool>) {
+        println!("DNA Animation");
+        println!("Press Ctrl-C to quit...");
+
+        let r = running.clone();
+        ctrlc::set_handler(move || {
+            r.store(false, Ordering::SeqCst);
+        })
+        .expect("Error setting Ctrl-C handler");
+
+        self.animation.animate(&running);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("DNA Animation");
+    println!("Press Ctrl-C to quit...");
+
+    let dna = Dna {
+        pause: Duration::from_millis(150),
+        rows: [
+            "         ##",
+            "        #{}-{}#",
+            "       #{}---{}#",
+            "      #{}-----{}#",
+            "     #{}------{}#",
+            "    #{}------{}#",
+            "    #{}-----{}#",
+            "     #{}---{}#",
+            "     #{}-{}#",
+            "      ##",
+            "     #{}-{}#",
+            "     #{}---{}#",
+            "    #{}-----{}#",
+            "    #{}------{}#",
+            "     #{}------{}#",
+            "      #{}-----{}#",
+            "       #{}---{}#",
+            "        #{}-{}#",
+        ],
+    };
+
+    let running = Arc::new(AtomicBool::new(true));
+
+    let app = App::new(Box::new(dna));
+    app.run(running);
 }
